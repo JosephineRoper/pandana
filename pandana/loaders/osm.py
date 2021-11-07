318ee0ffe3bf93ec818e4f03bbc2bd0916e3a848
JosephineRoper <61174184+JosephineRoper@users.noreply.github.com>
2021-11-07 21:47:52 +1100

    Added OSM way querying
    way_query, build_way_query and process_way

--- pandana/loaders/osm.py
+++ pandana/loaders/osm.py
@@ -198,3 +198,112 @@ def node_query(lat_min, lng_min, lat_max, lng_max, tags=None):
 
     nodes = [process_node(n) for n in node_data['elements']]
     return pd.DataFrame.from_records(nodes, index='id')
+
+def process_way(e):
+    """
+    Process a way element entry into a dict suitable for going into
+    a Pandas DataFrame.
+
+    Parameters
+    ----------
+    e : dict
+    Returns
+    -------
+    way : dict
+    """
+
+    uninteresting_tags = {
+        'source',
+        'source_ref',
+        'source:ref',
+        'history',
+        'attribution',
+        'created_by',
+        'tiger:tlid',
+        'tiger:upload_uuid',
+    }
+
+    way = {
+        'id': e['id'],
+        'lat': e['center']['lat'],
+        'lon': e['center']['lon']
+    }
+
+    if 'tags' in e:
+        for t, v in list(e['tags'].items()):
+            if t not in uninteresting_tags:
+                way[t] = v
+
+    return way
+
+def build_way_query(lat_min, lng_min, lat_max, lng_max, tags=None):
+    """
+    Build the string for a way-based OSM query.
+
+    Parameters
+    ----------
+    lat_min, lng_min, lat_max, lng_max : float
+    tags : str or list of str, optional
+        Way tags that will be used to filter the search.
+        See http://wiki.openstreetmap.org/wiki/Overpass_API/Language_Guide
+        for information about OSM Overpass queries
+        and http://wiki.openstreetmap.org/wiki/Map_Features
+        for a list of tags.
+
+    Returns
+    -------
+    query : str
+
+    """
+    if tags is not None:
+        if isinstance(tags, str):
+            tags = [tags]
+        tags = ''.join('[{}]'.format(t) for t in tags)
+    else:
+        tags = ''
+
+    query_fmt = (
+        '[out:json];'
+        '('
+        '  way'
+        '  {tags}'
+        '  ({lat_min},{lng_min},{lat_max},{lng_max});'
+        ');'
+        'out center;')
+
+    return query_fmt.format(
+        lat_min=lat_min, lng_min=lng_min, lat_max=lat_max, lng_max=lng_max,
+        tags=tags)
+
+
+def way_query(lat_min, lng_min, lat_max, lng_max, tags=None):
+    """
+    Search for OSM ways within a bounding box that match given tags.
+
+    Parameters
+    ----------
+    lat_min, lng_min, lat_max, lng_max : float
+    tags : str or list of str, optional
+        Way tags that will be used to filter the search.
+        See http://wiki.openstreetmap.org/wiki/Overpass_API/Language_Guide
+        for information about OSM Overpass queries
+        and http://wiki.openstreetmap.org/wiki/Map_Features
+        for a list of tags.
+
+    Returns
+    -------
+    ways : pandas.DataFrame
+        Will have 'lat' and 'lon' columns corresponding to the center of
+        the way's bounding box, plus other columns for the
+        tags associated with the way (these will vary based on the query).
+        Index will be the OSM way IDs.
+
+    """
+    way_data = make_osm_query(build_way_query(
+        lat_min, lng_min, lat_max, lng_max, tags=tags))
+
+    if len(way_data['elements']) == 0:
+        raise RuntimeError('OSM query results contain no data.')
+
+    ways = [process_way(n) for n in way_data['elements']]
+    return pd.DataFrame.from_records(ways, index='id')
